a1c2993b2ce6f0362f927bfed4cb5b8e6153987b
Washing-Mr8 <washing.22ml@gmail.com>
2023-04-19 11:27:46 +0200

    DAOProv y test

--- bocateriaaaCodigo/test/Integracion/Proveedores/DAOProvImpTest.java
+++ bocateriaaaCodigo/test/Integracion/Proveedores/DAOProvImpTest.java
@@ -1,4 +1,4 @@
-package Integracion.Empleados;
+package Integracion.Proveedores;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,6 +8,7 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,18 +16,20 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
+import Integracion.Empleados.DAOEmpleados;
 import Integracion.FactoriaIntegracion.FactoriaIntg;
-import Integracion.Marcas.DAOMarcas;
 import Negocio.Empleados.TEmpleados;
-import Negocio.Empleados.TEmpleadosTP;
-import Negocio.Marcas.TMarcas;
 import Negocio.Empleados.TEmpleadosTC;
+import Negocio.Empleados.TEmpleadosTP;
+import Negocio.Proveedores.TProvComunitario;
+import Negocio.Proveedores.TProvNacional;
+import Negocio.Proveedores.TProveedores;
 
 
-class DAOEmpImpTest{
+class DAOProvImpTest {
 	
-	private final static String _path = "resources/empleados/emp.JSON";
-	
+	private final static String _path = "resources/proveedores/proveedores.JSON";
+
 	public void openFile() {
 		try(BufferedWriter salida = new BufferedWriter(new FileWriter(_path))){
 			salida.close();
@@ -40,56 +43,50 @@ class DAOEmpImpTest{
 	@Test
 	public void comportamientoBasico() {
 		FactoriaIntg factoriaIntegracion = FactoriaIntg.getInstance();
-		DAOEmpleados daoEmp = factoriaIntegracion.generarDAOEmpleados();
+		DAOProv daoProv = factoriaIntegracion.generarDAOProv();
 		
 		openFile();
-		
-		//jornada == 0 -> Empleado a Tiempo Parcial
-		//jornada == 1-> Empleado a Tiempo Completo
 
-		TEmpleados p1 = new TEmpleadosTC ("Prueba", "numero1", "123X", -1, 1, 1, true, 800);
-		TEmpleados p2 = new TEmpleadosTP ("Prueba", "numero2", "321X", -1, 0, 1, true,4 ,20 );		
+		TProveedores p1 = new TProvComunitario (-1, "Prueba1", 0, true, "Italia");
+		TProveedores p2 = new TProvNacional (-1, "Prueba2", 0, true, "Huelva");
 		int id;
 		
 		//comprobamos que devuelva el id correcto
-		id = daoEmp.create(p1);
+		id = daoProv.create(p1);
 		assertEquals(1,id,"No ha devuelto el id que acaba de crear");
 	
-		id = daoEmp.create(p2);
+		id = daoProv.create(p2);
 		
 		//deberian ser iguales el dept 1 y el que se manda a leer por el nombre
 		//assertEquals(p1,daoMarca.readByName("Prueba1"));
 		
-		TEmpleados p3 = new TEmpleadosTC ("Prueba", "numero3", "132X", -1, 1, 1, true, 1000);
+		TProveedores p3 = new TProvComunitario (-1, "Prueba3", 0, true, "Alemania");
 		
-		daoEmp.create(p3);
+		daoProv.create(p3);
 		
 		p3.setNombre("ABC");
 		
-		daoEmp.update(p3);
+		daoProv.update(p3);
 		//comprobamos que funciona el update
-		assertEquals("ABC", daoEmp.read(3).getNombre());
+		assertEquals("ABC", daoProv.read(3).getNombre());
 	//	assertEquals("nuevaSede", daoMarca.read(3).getSede());
 		
-		List<TEmpleados> empList = new ArrayList<TEmpleados>();
+		List<TProveedores> provList = new ArrayList<TProveedores>();
 		
-		empList = daoEmp.readAll();
+		provList = daoProv.readAll();
 		//comprobamos que lee bien el fichero
-		assertTrue(empList.size() == 3); //comprobamos que lea todo bien
+		assertTrue(provList.size() == 3); //comprobamos que lea todo bien
 		
-		assertTrue(empList.get(0) instanceof TEmpleadosTC); //el primer empleado esta a tiempo completo
-		assertTrue(empList.get(1) instanceof TEmpleadosTP);//el segundo empleado esta a tiempo parcial 
+		assertTrue(provList.get(0) instanceof TProvComunitario);
+		assertTrue(provList.get(1) instanceof TProvNacional); 
 
 		
-		id = daoEmp.delete(2);
+		id = daoProv.delete(2);
 		assertEquals(2, id); //devuelve el id que ha borrado
 		//actualizamos la lista y comprobamos que se ha borrado
-		empList = daoEmp.readAll();
-		assertTrue(empList.size() == 3); //el tamaño de la lista no disminuye
-		assertTrue(empList.get(1).getActivo() == false);	
+		provList = daoProv.readAll();
+		assertTrue(provList.size() == 3); //el tamaÃ±o de la lista no disminuye
+		assertTrue(provList.get(1).getActivo() == false);	
 	}
 
-	
-	
-	
 }
